522ff639d5e2fbb4a621e43b4541e88fa4d0df25
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-27 00:12:11 +0530

    update..

--- app/src/main/java/com/example/bmical/MainActivity.kt
+++ app/src/main/java/com/example/bmical/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.bmical
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
